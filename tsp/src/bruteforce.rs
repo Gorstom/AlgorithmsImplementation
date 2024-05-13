f5b47358417d4ed0b58aff91dc1509f3ad40f99c
Spartkaktus <97987108+Spartkaktus@users.noreply.github.com>
2024-05-13 14:32:29 +0200

    Update bruteforce.rs

--- tsp/src/bruteforce.rs
+++ tsp/src/bruteforce.rs
@@ -3,7 +3,7 @@ pub fn calculate_tour_length(tour: &(Vec<usize>, i32), distances: &[Vec<i32>]) -
     for i in 0..tour.0.len() - 1 {
         length += distances[tour.0[i]][tour.0[i + 1]];
     }
-    length += distances[tour.0[tour.0.len() - 1]][tour.0[0]]; // wróć do miasta początkowego
+    length += distances[tour.0[tour.0.len() - 1]][tour.0[0]]; // return to the start city
     length
 }
 
@@ -31,12 +31,12 @@ pub(crate) fn brute_force_tsp(distances: &[Vec<i32>]) -> (Vec<usize>, i32) {
     let num_cities = distances.len();
 
     let mut current_tour: Vec<usize> = (1..num_cities).collect();
-    let mut optimal_tour = current_tour.clone(); // na początku trasa optymalana jest równa pierwszej permutacji
+    let mut optimal_tour = current_tour.clone(); // at the beginning of the optimal route is equal to the first permutation
 
-    // Zainicjuj optymalną odległość, podając początkową długość trasy
+    // Initialize the optimal distance by specifying the initial route length
     let mut optimal_distance = calculate_tour_length(&(optimal_tour.clone(), 0), distances);
 
-    // Zacznij od drugiego miasta, ponieważ miasto startowe jest stałe
+    // Start with the second city because the starting city is fixed
     while next_permutation(&mut current_tour) {
         let current_distance = calculate_tour_length(&(current_tour.clone(), 0), distances);
         if current_distance < optimal_distance {
@@ -45,7 +45,7 @@ pub(crate) fn brute_force_tsp(distances: &[Vec<i32>]) -> (Vec<usize>, i32) {
         }
     }
 
-    // dodaj miasto startowe do łącznej trasy
+    // add the starting city to the total route
     optimal_tour.insert(0, 0);
 
     (optimal_tour, optimal_distance)
